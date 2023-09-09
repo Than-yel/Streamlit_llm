e560bf39892d5fa283c64e414eff9d9d0d7aa3b2
Ndukwe Daniel <125179701+Than-yel@users.noreply.github.com>
2023-09-09 03:23:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("My Name is Daniel?"):
+if prompt := st.chat_input("I'm Prophet Ikuku. How may i assist you?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
